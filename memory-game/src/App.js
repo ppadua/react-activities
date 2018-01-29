ce6fdc82b6dd41dbb76f9d5474df4481f267c113
ppadua <ppadua@village88.com>
2018-01-29 08:04:06 +0800

    Memory game activity initial

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,180 +1,83 @@
 import React, { Component } from 'react';
 import './App.css';
 import './assets/css/index.css';
-var FontAwesome = require('react-fontawesome');
-
 
 class Todo extends Component {
 	constructor(props) {
 		super(props);
-		this.counter_index = 1;
-
 		this.state = {
 			items: [
-				{ id: 1, text: "Learn React", completed: true, is_edit : false },
-				{ id: 2, text: "Build a todo app", completed: false, is_edit : false },
-				{ id: 3, text: "Profit", completed: false, is_edit : false }
+				0,1,0,1,0,0,1,0,0,1,0,0
 			],
-			filter: 'all',
-			completeAll: true,
-			newTodoText: ''
+			is_flip : false,
+			is_game_over : false,
+			wrong_card_index : 0
 		}
 
-		this.showTodo = this.showTodo.bind(this);
-		this.handleChange = this.updateNewTodoText.bind(this);
-		this.updateNewTodoText = this.updateNewTodoText.bind(this);
-		this.addTodo = this.addTodo.bind(this);
-		this.prepareTodo = this.prepareTodo.bind(this);
-		this.removeTodo = this.removeTodo.bind(this);
-		this.checkedUnchekedAll = this.checkedUnchekedAll.bind(this);
-		this.clearCompleted = this.clearCompleted.bind(this);
+		this.showGame = this.showGame.bind(this)
+		this.startGame = this.startGame.bind(this)
+		this.selectedCard = this.selectedCard.bind(this)
 	}
 
-	addTodo(text){
-		text.preventDefault()
+	startGame(){
+		var new_items = this.state;
+		new_items.is_flip = true;
+		this.setState(new_items);
 
-		if(this.state.newTodoText.trim() !== ""){
-			var state = this.state;
+		setTimeout(() => {
+			var new_items = this.state;
+			new_items.is_flip = false;
 
-			state.items.push({
-				id : state.items.length + 1,
-				text : this.state.newTodoText,
-				completed : false
-			});
-
-			state.newTodoText = "";
-
-			this.setState(state);
-		}
+			this.setState(new_items);
+		}, 3000)
 	}
 
-	updateTodo(todoId, status){
-		var item = this.state.items;
-
-		item[item.map(function(item) { return item.id; }).indexOf(todoId)].text = status.target.value;
-
-		this.setState(item);
-	}
-
-	updateFilter(filter){
-		var state = this.state;
-		state.filter = filter;
-		this.setState(state);
-	}
-
-	updateNewTodoText(text){
-		this.setState({newTodoText: text.target.value});
-	}
-
-	handleEditText(todoId, event){
-		var item = this.state.items;
-		item[item.map(function(item) { return item.id; }).indexOf(todoId)].is_edit =  (event === "click" ? true : false);
-		this.setState(item);
-	}
-
-
-	showTodo(){
-		var item = this.state.items;
-		// var li_append = [];
-        //
-		// for (var i = 0; i < item.length; i++) {
-		// 	li_append.push(<this.prepareTodo items={item[i]} key={i}/>)
-		// }
-
-		// return li_append;
-
-		var filterTodo = item.filter(function (todo) {
-			switch (this.state.filter) {
-				case "active":
-					return !todo.completed;
-				case "completed":
-					return todo.completed;
-				default:
-					return true;
+	selectedCard(event){
+		console.log(this.state)
+		if(this.state.wrong_card_index != 2){
+			if(event.target.getAttribute("value") === "true"){
+				event.target.classList.add("selected_correct_card");
+				event.target.classList.remove("correct_start_card");
 			}
-		}, this);
+			else {
+				event.target.classList.add("selected_wrong_card")
+				var new_items = this.state;
+				new_items.wrong_card_index++;
 
-		return	filterTodo.map((props) =>
-			<li key={props.id} data-id={props.id}>
-				<label htmlFor={"checked_"+props.id}>
-					<FontAwesome className={props.completed ? "checked_green" : "checked_gray"}name='check-circle-o' size='2x'/>
-					<input type="checkbox" name="completed" checked={props.completed} onChange={() => this.checkedTodo(props.id)} id={"checked_"+props.id} className="hidden" />
-				</label>
-				<span onDoubleClick={() => this.handleEditText(props.id, "click")} className={"unedited_todo"+ (props.completed ? ' strike' : '')+" "+(props.is_edit ? "hidden" : " ") }> {props.text}</span>
-				<input className={(props.is_edit) ? "edited_input_todo" : "hidden"} type="text" value={props.text}  onMouseOut={() => this.handleEditText(props.id, "mouseout")} onChange={this.updateTodo.bind(this, props.id)}/>
-				<span className="removetodo" onClick={() => this.removeTodo(props.id)}><FontAwesome name='times' size='lg'/></span>
-			</li>
-		);
+				this.setState(new_items);
+			}
+		}
+		else{
+			var new_items = this.state;
+			new_items.is_game_over = true;
+
+			this.setState(new_items);
+		}
 	}
 
-	removeTodo(todoId){
-		var item = this.state.items;
-	 	item.splice(item.map(function(item) { return item.id; }).indexOf(todoId), 1);
-		this.setState(item);
-	}
-
-	checkedTodo(todoId){
-		var item = this.state.items;
-		var is_checked = item[item.map(function(item) { return item.id; }).indexOf(todoId)].completed;
-
-		item[item.map(function(item) { return item.id; }).indexOf(todoId)].completed = (is_checked ? false : true);
-
-		this.setState(item);
-	}
-
-	checkedUnchekedAll(event){
-		this.state.items.map((props) => props.completed =( event.target.getAttribute("value")  === "true" ? true : false));
-		this.state.completeAll = ( event.target.getAttribute("value") === "true") ? false : true;
-		this.setState(this.state);
-	}
-
-	clearCompleted(){
-		var item = this.state.items;
-
-		var filterTodo = item.filter(function (todo) {
-			return !todo.completed;
-		}, this);
-
-		this.state.items = filterTodo
-
-		this.setState(item);
-	}
-
-	prepareTodo(props){
-		// 	return (
-		// 		<li key={props.items.id} data-id={props.items.id}>
-		// 			<input type="checkbox" name="completed" checked={props.items.completed} onChange={this.checkedTodo}/>
-		// 			<span onDoubleClick={() => this.handleEditText(props.items.id, "click")} className={"unedited_todo"+ (this.props.completed ? 'strike' : '')+" "+(props.items.is_edit ? "hidden" : " ") }> {props.items.text}</span>
-		// 			<input className={(props.items.is_edit) ? "edited_input_todo" : "hidden"} type="text" value={props.items.text}  onMouseOut={() => this.handleEditText(props.items.id, "mouseout")} onChange={this.updateTodo.bind(this, props.items.id)}/>
-		// 			<span onClick={() => this.removeTodo(props.items.id)}>x</span>
-		// 		</li>
-		// 	);
+	showGame(){
+		console.log(this.state)
+		return	<ul id="all_cards">
+			{
+				this.state.items.map(
+					(props, i) =>
+						<li className={"cards"+ (props === 1 && this.state.is_flip === true ? " correct_card" : "") +" "+ (props === 1 && this.state.is_flip === false && this.state.is_game_over === true ? 'correct_start_card' : "" )}
+							key={i}
+							value={props === 1 ? true : false }
+							onClick={!this.state.is_flip && !this.state.is_game_over? this.selectedCard : undefined} >
+						</li>
+				)
+			}
+		</ul>
 	}
 
 	render() {
 		return (
 			<div>
-				<form action="" onSubmit={this.addTodo}>
-					<span onClick={this.checkedUnchekedAll}><FontAwesome className='super-crazy-colors'	name='angle-down' size='2x' value={this.state.completeAll} /></span>
-					<input type="text" name="text" value={this.state.newTodoText} onChange={this.updateNewTodoText} placeholder="What needs to be done"/>
-				</form>
-				<ul id="todos">
-					{this.showTodo()}
-				</ul>
-				<div>
-					<span>{this.state.items.length} items left</span>
-					<ul id="filter">
-						<li onClick={() => this.updateFilter("all")} className={this.state.filter === "all" ? "active" : ""}>All</li>
-						<li onClick={() => this.updateFilter("active")} className={this.state.filter === "active" ? "active" : ""}>Active</li>
-						<li onClick={() => this.updateFilter("completed")} className={this.state.filter === "completed" ? "active" : ""}>Completed</li>
-					</ul>
-					<span className="clear_completed" onClick={this.clearCompleted}>Clear Completed</span>
-				</div>
-				<div>Double click to edit</div>
+				<this.showGame />
+				<button onClick={this.startGame}>Start</button>
 			</div>
 		);
-
-
 	}
 }
 
--- memory-game/src/index.js
+++ memory-game/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloDojo from './App';
 import registerServiceWorker from './registerServiceWorker';
-var FontAwesome = require('react-fontawesome');
 
 
 ReactDOM.render(<HelloDojo />, document.getElementById('todo'));
